3a5f5ee6674796e1ec23c10f0e7d0c9dc415c3b6
swoo1226 <46513548+swoo1226@users.noreply.github.com>
2022-06-17 21:56:07 +0900

    showing posts by animation

--- pages/posts/index.jsx
+++ pages/posts/index.jsx
@@ -6,23 +6,54 @@ import homeStyles from "../../styles/Home.module.scss";
 import { getSortedPostsData } from "../../lib/posts";
 import { gql } from "@apollo/client";
 import client from "../../apollo-client";
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
 
 export default function Posts({ allPostsData, countries }) {
   return (
     <main className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
       <h2 className={utilStyles.headingLg}>Posts</h2>
-      <ul className={classNames(utilStyles.list, homeStyles.flexbox)}>
+      <motion.ul
+        className={classNames(utilStyles.list, homeStyles.flexbox, "container")}
+        variants={container}
+        initial="hidden"
+        animate="visible"
+      >
         {allPostsData.map(({ id, date, title }) => (
-          <li
-            className={classNames(utilStyles.listItem, utilStyles.glass)}
+          <motion.li
+            className={classNames(
+              utilStyles.listItem,
+              utilStyles.glass,
+              "item"
+            )}
             key={id}
+            variants={item}
           >
             <Link href={`/posts/${encodeURIComponent(id)}`}>{title}</Link>
             <br />
             {date}
-          </li>
+          </motion.li>
         ))}
-      </ul>
+      </motion.ul>
     </main>
   );
 }
